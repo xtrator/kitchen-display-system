b9cff83f72e042c10ca99eeabd7ce21cf1c9e3c2
Alexander Luna <alexfox_2000@hotmail.com>
2023-05-23 10:34:24 -0500

    remove boilerplate css and assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styled from 'styled-components'
+
+const Button = styled.button``
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -28,6 +20,8 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <hr/>
+      <Button>Hola mundo</Button>
     </>
   )
 }
