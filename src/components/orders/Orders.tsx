f380e5736c74810152c9ac40f0bb082301a51a61
Alexander Luna <alexfox_2000@hotmail.com>
2023-05-25 18:08:45 -0500

    ADD way to filter orders by status

--- src/components/orders/Orders.tsx
+++ src/components/orders/Orders.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import Order from "./Order";
 import styled, { css } from "styled-components";
 
@@ -12,6 +13,7 @@ interface Props {
     orderTime: string;
     estimate: number;
     table: number;
+    status: string;
   }[];
 }
 
@@ -36,19 +38,36 @@ const StyledButton = styled.button<{ $color?: string }>`
 `;
 
 export default function Orders({ orders }: Props) {
+  const [filter, setFilter] = useState("");
   if (orders.length === 0)
     return <h1>Looks like you're empty, create your first order.</h1>;
+
+  const filteredOrders = orders.filter((o) => {
+    if (filter === "") return o;
+
+    if (o.status === filter) return o;
+  });
   return (
     <>
       <div>
-        <StyledButton $color={"blue"}>All</StyledButton>
-        <StyledButton $color={"blue"}>Pending</StyledButton>
-        <StyledButton $color={"blue"}>Preparing</StyledButton>
-        <StyledButton $color={"blue"}>Completed</StyledButton>
-        <StyledButton $color={"blue"}>Cancelled</StyledButton>
+        <StyledButton $color={"blue"} onClick={() => setFilter("")}>
+          All
+        </StyledButton>
+        <StyledButton $color={"blue"} onClick={() => setFilter("pending")}>
+          Pending
+        </StyledButton>
+        <StyledButton $color={"blue"} onClick={() => setFilter("preparing")}>
+          Preparing
+        </StyledButton>
+        <StyledButton $color={"blue"} onClick={() => setFilter("completed")}>
+          Completed
+        </StyledButton>
+        <StyledButton $color={"blue"} onClick={() => setFilter("cancelled")}>
+          Cancelled
+        </StyledButton>
       </div>
       <OrdersWrapper>
-        {orders.map((order) => {
+        {filteredOrders.map((order) => {
           /* @ts-ignore */
           return <Order order={order} key={order.docId} />;
         })}
